67dc21a0742cd036fe14646c55508e4c55f9a1f5
Inyene Etuk <idetuk@ncsu.edu>
2022-04-12 16:38:24 -0400

    [Implementation] Partially implemented Notebook

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
@@ -2,9 +2,13 @@ package edu.ncsu.csc216.wolf_tasks.model.notebook;
 
 import java.io.File;
 
+import edu.ncsu.csc216.wolf_tasks.model.io.NotebookWriter;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.ActiveTaskList;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+import edu.ncsu.csc216.wolf_tasks.model.util.ISortedList;
+import edu.ncsu.csc216.wolf_tasks.model.util.SortedList;
 
 /**
  * Notebook class for WolfTasks, this is a Notebook that is comprised of TaskLists that have Tasks.
@@ -20,6 +24,15 @@ public class Notebook {
 
 	/** Whether or not a notebook has been changed */
 	private Boolean isChanged;
+	
+	/** List of sorted taskLists */
+	private ISortedList<TaskList> taskLists ;
+	
+	/** List of active tasks */
+	private ActiveTaskList activeTaskList;
+	
+	/** List of current tasks */
+	private AbstractTaskList currentTaskList;
 
 	/**
 	 * Constructor for Notebook
@@ -28,8 +41,14 @@ public class Notebook {
 	 * @throws IllegalArgumentException for invalid name
 	 */
 	public Notebook(String name) {
+		if (name == null || name.isEmpty() || "Active Tasks".equals(name)) {
+			throw new IllegalArgumentException();
+		}
 		setNotebookName(name);
-		setChanged(false);
+		taskLists = new SortedList<TaskList>();
+		activeTaskList = new ActiveTaskList();
+		currentTaskList = activeTaskList;
+		setChanged(true);
 	}
 
 	/**
@@ -37,8 +56,9 @@ public class Notebook {
 	 * 
 	 * @param file the file to output to
 	 */
-	public void saveNotebook(File file) {
-
+	public void saveNotebook(File notebookFile) {
+		NotebookWriter.writeNotebookFile(notebookFile, notebookName, taskLists);
+		setChanged(false);
 	}
 
 	/**
@@ -85,7 +105,17 @@ public class Notebook {
 	 * @throws IllegalArgumentException for invalid name
 	 */
 	public void addTaskList(TaskList taskList) {
-
+		if (taskList.getTaskListName().equals("Active Tasks")) {
+			throw new IllegalArgumentException("Invalid name");
+		}
+		for (int i = 0; i < this.taskLists.size(); i++) {
+			if (this.taskLists.get(i).getTaskListName().equals(taskList.getTaskListName())) {
+				throw new IllegalArgumentException("Invalid name");
+			}
+		}
+		this.taskLists.add(taskList);
+		this.currentTaskList = taskList;
+		setChanged(true);
 	}
 
 	/**
@@ -94,25 +124,43 @@ public class Notebook {
 	 * @return an array of Strings of the names of the TaskLists in a Notebook
 	 */
 	public String[] getTaskListsNames() {
-		getActiveTaskList();
-		return null;
+		String[] names = new String[this.taskLists.size()];
+		names[0] = this.activeTaskList.getTaskListName();
+		for (int i = 0; i < this.taskLists.size() - 1; i++) {
+			names[i+1] = this.taskLists.get(i).getTaskListName();
+		}
+		return names;
 	}
 
 	/**
-	 * Shows the active TaskList on the Controller
+	 * Rebuilds activeTaskList by iterating through taskLists and
+	 * adding each active task (by priority)
 	 * 
 	 */
 	private void getActiveTaskList() {
-
+		this.activeTaskList = new ActiveTaskList();
+		for (int i = 0; i < this.taskLists.size(); i++) {
+			TaskList list = taskLists.get(i);
+			for (int j = 0; j < list.getTasks().size(); j++) {
+				if(list.getTask(j).isActive()) {
+					this.activeTaskList.addTask(list.getTask(j));
+				}
+			}
+		}
 	}
 
 	/**
-	 * Sets the name of a current TaskList
+	 * Sets the currentTaskList to the AbstractTaskList with the give name
 	 * 
 	 * @param name the name to set a CurrentTaskList to
 	 */
 	public void setCurrentTaskList(String name) {
-
+		for (int i = 0; i < this.taskLists.size(); i++) {
+			if (name.equals(taskLists.get(i).getTaskListName())) {
+				this.currentTaskList = taskLists.get(i);
+			}
+		}
+		this.currentTaskList = this.activeTaskList;
 	}
 
 	/**
@@ -125,13 +173,16 @@ public class Notebook {
 	}
 
 	/**
-	 * Edits the current task list with a name
+	 * Updates currenTaskList with name
 	 * 
 	 * @param name the parameter to edit a task with
 	 * @throws IllegalArgumentException for invalid name
 	 */
 	public void editTaskList(String name) {
-
+		//checks if currentTaskList is an activeTaskList
+		if (this.currentTaskList.getClass() == this.activeTaskList.getClass()) {
+			
+		}
 	}
 
 	/**
