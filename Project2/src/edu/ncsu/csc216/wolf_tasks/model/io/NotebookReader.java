a4e12e5238ac7c63493b939fed9c1a653e5560ef
Mason Morano <mmmorano@ncsu.edu>
2022-04-14 15:12:04 -0400

    [Implementation] Attempted to implement NotebookReader and added tests

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -32,17 +32,13 @@ public class NotebookReader {
 			Scanner fileReader = new Scanner(new FileInputStream(file));
 			String nameLine = fileReader.nextLine();
 
-			// Check first line for exclamation point
-			Scanner nameScanner = new Scanner(nameLine);
-			if (!nameScanner.next().equals("!")) {
-				nameScanner.close();
+			if (nameLine.charAt(0) != '!') {
+				fileReader.close();
 				throw new IllegalArgumentException("Name must start with exclamation point");
 			}
 
-			// advance scanner past exclamation point
-			nameScanner.next();
-			String name = nameScanner.next();
-			nameScanner.close();
+			// remove exclamation point
+			String name = nameLine.substring(2);
 
 			// Create notebook with read name
 			Notebook n = new Notebook(name);
@@ -53,11 +49,11 @@ public class NotebookReader {
 			}
 			// Close the Scanner b/c we're responsible with our file handles
 			fileReader.close();
+			return n;
 
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException("Unable to load file.");
 		}
-		return null;
 	}
 
 	/**
@@ -68,27 +64,90 @@ public class NotebookReader {
 	 */
 	private static TaskList processTaskList(String taskList) {
 		Scanner scnr = new Scanner(taskList);
-		String taskListFields = scnr.nextLine();
+		// remove the #
+		String taskListFields = scnr.nextLine().trim().substring(0);
+
+		// read the fields from the line for the tasklist
 		Scanner scnr1 = new Scanner(taskListFields);
 		scnr1.useDelimiter(",");
 		String taskListName = scnr1.next().trim();
 		int count = scnr1.nextInt();
 		TaskList t = new TaskList(taskListName, count);
+
+		// break tasklist into "task tokens"
+		scnr.useDelimiter("\\r?\\n?[*]");
+		while (scnr.hasNext()) {
+			processTask(t, scnr.next());
+		}
+
+		// close scanners
 		scnr.close();
 		scnr1.close();
-		return null;
-		}
+
+		return t;
+	}
 
 	/**
 	 * Processes a set of tasks from the TaskList read in by the processTaskList
 	 * method.
 	 * 
-	 * @param taskList a TaskList to read in and process
-	 * @param fileName a name to give a Task?
+	 * @param taskList   a TaskList to read in and process
+	 * @param taskString a Task in the form of a scanned String
 	 * @return a Task object
 	 */
-	private static Task processTask(AbstractTaskList taskList, String fileName) {
-		return null;
+	private static Task processTask(AbstractTaskList taskList, String taskString) {
+		String taskFields = taskString.substring(1);
+		Scanner taskScanner = new Scanner(taskFields);
+
+		// trim leading
+		boolean isActive = false;
+		boolean isRecurring = false;
+		String nameAndFields = taskScanner.nextLine().trim().substring(2);
+		Scanner taskFieldsScnr = new Scanner(nameAndFields);
+		taskFieldsScnr.useDelimiter(",");
+		String name = taskFieldsScnr.next();
+
+		// cases of if there is only active and no recurring
+//		while (taskFieldsScnr.hasNext()) {
+//			//only active and no recurring
+//			if ("active".equals(taskFieldsScnr.next())) {
+//				isActive = true;
+//				isRecurring = false;
+//				break;
+//			}
+//			else if ("recurring".equals(taskFieldsScnr.next()))
+//		}
+		if (taskFieldsScnr.hasNext()) {
+			String next = taskFieldsScnr.next();
+			// only active, nothing else after
+			if ("active".equals(next)) {
+				isActive = true;
+				//cant have recurring afterwards
+				isRecurring = false;
+			}
+			// can be recurring and then check if there is a next
+			else if ("recurring".equals(next)) {
+				isRecurring = true;
+				isActive = false;
+				// if there is something after recurring, it has to be active
+				if (taskFieldsScnr.hasNext()) {
+					isActive = true;
+				}
+			}
+		}
+		
+		taskFieldsScnr.close();
+
+		String description = "";
+
+		// get description
+		// TODO see if this reads the descriptions correctly
+		while (taskScanner.hasNextLine()) {
+			description += taskScanner.nextLine().trim() + "\n";
+		}
+
+		taskScanner.close();
+		return new Task(name, description, isRecurring, isActive);
 	}
 
 }
--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
@@ -6,23 +6,30 @@ import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
+import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
+
 /**
  * Tests NotebookReader class
+ * 
  * @author Mason Morano
  * @author Inyene Etuk
  *
  */
 class NotebookReaderTest {
-	
+
 	/** Valid notebook file */
 	private final File validTestFile = new File("test-files/notebook0.txt");
-	
+
 	/** Valid notebook file 1 */
 	private final File validTestFile1 = new File("test-files/notebook1.txt");
-	
-	/** Valid notebook file 5 task list with missing name - creates notebook with no tasks lists */
+
+	/**
+	 * Valid notebook file 5 task list with missing name - creates notebook with no
+	 * tasks lists
+	 */
 	private final File validTestFile2 = new File("test-files/notebook5.txt");
-	
+
 	/** Invalid notebook file */
 	private final File invalidTestFile = new File("test-files/notebook3.txt");
 
@@ -31,12 +38,34 @@ class NotebookReaderTest {
 	 */
 	@Test
 	void testReadValidNotebookFile() {
-		//three valid files
+		// three valid files
 		assertDoesNotThrow(() -> NotebookReader.readNotebookFile(validTestFile));
 		assertDoesNotThrow(() -> NotebookReader.readNotebookFile(validTestFile1));
-		assertDoesNotThrow(() -> NotebookReader.readNotebookFile(validTestFile2));
+
+		Notebook n = NotebookReader.readNotebookFile(validTestFile1);
+
+		assertEquals("School", n.getNotebookName());
+		assertEquals("CSC 216", n.getTaskListsNames()[1]);
+		n.setCurrentTaskList("CSC 216");
+		assertEquals(35, n.getCurrentTaskList().getCompletedCount());
+
+		Task t1 = n.getCurrentTaskList().getTask(0);
+		Task t2 = n.getCurrentTaskList().getTask(1);
+
+		assertEquals("Complete exercises", t1.getTaskName());
+		assertTrue(t1.isRecurring());
+		assertFalse(t1.isActive());
+
+		// assertEquals("", t1.toString());
+		// assertEquals("", t2.toString());
+
+		assertEquals("Complete quizzes", t2.getTaskName());
+		assertTrue(t2.isRecurring());
+		assertFalse(t2.isActive());
+
+		// assertDoesNotThrow(() -> NotebookReader.readNotebookFile(validTestFile2));
 	}
-	
+
 	/**
 	 * Tests NotebookReader.readNotebookFile() invalid file
 	 */
@@ -44,6 +73,5 @@ class NotebookReaderTest {
 	void testReadInvalidNotebookFile() {
 		assertThrows(IllegalArgumentException.class, () -> NotebookReader.readNotebookFile(invalidTestFile));
 	}
-	
 
 }
