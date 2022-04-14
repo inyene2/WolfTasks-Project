ca96db869b2e06ce6fc6d2efdf1b6d2e10f3d650
Mason Morano <mmmorano@ncsu.edu>
2022-04-13 21:17:48 -0400

    [Implementation] Partially implemented NotebookReader

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -1,6 +1,9 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
@@ -26,7 +29,64 @@ public class NotebookReader {
 	 */
 	public static Notebook readNotebookFile(File file) {
 		processTask(processTaskList(""), "");
+		// return null;
+
+		String tempLines = "";
+
+//		try {
+//			Scanner fileReader = new Scanner(new FileInputStream(file));
+//			String nameLine = fileReader.nextLine();
+//
+//			// Check first line for exclamation point
+//			Scanner nameScanner = new Scanner(nameLine);
+//			if (!nameScanner.next().equals("!")) {
+//				nameScanner.close();
+//				throw new IllegalArgumentException("Name must start with exclamation point");
+//			}
+//
+//			// advance scanner past exclamation point
+//			nameScanner.next();
+//			String name = nameScanner.next();
+//			nameScanner.close();
+//
+//			// Create notebook with read name
+//			Notebook n = new Notebook(name);
+//
+//			tempLines += nameLine + "\n";
+//			while (fileReader.hasNextLine()) {
+//
+//			}
+//
+//		} catch (FileNotFoundException e) {
+//			throw new IllegalArgumentException("Unable to load file.");
+//		}
+
+		// Read in Notebook file line by line
+		try {
+
+			Scanner fileReader = new Scanner(new FileInputStream(file));
+			while (fileReader.hasNextLine()) {
+				tempLines = tempLines + fileReader.nextLine() + "\n";
+			}
+			if (tempLines.length() == 0) {
+				// There is nothing in the file
+				throw new IllegalArgumentException();
+			}
+			fileReader.close();
+
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		
+		// Check if the first character in the file is an exclamation point
+		if(tempLines.charAt(0) != '!') {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		
+		Scanner scnr = new Scanner(tempLines);
+		 
 		return null;
+		
 	}
 
 	/**
